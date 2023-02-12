8e9b68a25e5a72c332811216493fd1f343a02094
Taha Ansari <farhan974u@gmail.com>
2023-02-12 21:56:28 +0530

    Added home_controller and its corresponding route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express express routers
+app.use('/', require('./routes'));
+console.log('router loaded');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
